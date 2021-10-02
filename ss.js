d60a4da62b082f5115606e916712073036a0a592
iamjojohilvano <jojohilvano24@gmail.com>
2021-10-02 21:48:16 +0800

    added map style and infowindow on the location

--- ss.js
+++ ss.js
@@ -2,19 +2,57 @@
 
 
 function initMap(){
-  let mylocation= {lat: 41.883100, lng: -87.634350};   //variable to set marker location
+      let mylocation= {lat: 41.883100, lng: -87.634350};   //variable to set marker location
 
-  let map = new google.maps.Map(document.getElementById('map'), {  
-      center:mylocation,
-      zoom: 2
-  });   
+      let map = new google.maps.Map(document.getElementById('map'), {  
+          center: mylocation,
+          zoom: 2,
+          mapId: "f13cb0fcf7cfdb03"
+      });   
 
-   //market object
-  //marker of your coordinates OR location called "myLocationEastRemo"
-  let marker = new google.maps.Marker( {
-        position: mylocation,   
-        map: map,
-  });
+      const contentString =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+      '<div id="bodyContent">' +
+      "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+      "sandstone rock formation in the southern part of the " +
+      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+      "south west of the nearest large town, Alice Springs; 450&#160;km " +
+      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+      "Aboriginal people of the area. It has many springs, waterholes, " +
+      "rock caves and ancient paintings. Uluru is listed as a World " +
+      "Heritage Site.</p>" +
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+      "(last visited June 22, 2009).</p>" +
+      "</div>" +
+      "</div>";
+
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+
+   
+
+      //market object
+      //marker of your coordinates OR location called "myLocationEastRemo"
+      let marker = new google.maps.Marker( {
+            position: mylocation,   
+            map: map,
+      });
+
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        });
+      });
+
 };
 
 
