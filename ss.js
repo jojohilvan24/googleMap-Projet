d48cf9f090463d50594578ab9371f35632acdb15
iamjojohilvano <jojohilvano24@gmail.com>
2021-10-03 20:39:18 +0800

    modified the info window content for a singe marker

--- ss.js
+++ ss.js
@@ -6,31 +6,17 @@ function initMap(){
 
       let map = new google.maps.Map(document.getElementById('map'), {  
           center: mylocation,
-          zoom: 2,
+          zoom: 4,
           mapId: "f13cb0fcf7cfdb03"
       });   
 
       const contentString =
-      '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      '<div id="bodyContent">' +
-      "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      "sandstone rock formation in the southern part of the " +
-      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      "Aboriginal people of the area. It has many springs, waterholes, " +
-      "rock caves and ancient paintings. Uluru is listed as a World " +
-      "Heritage Site.</p>" +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-      "(last visited June 22, 2009).</p>" +
-      "</div>" +
-      "</div>";
+        '<div class="content">' +
+              '<a href="http://www.google.com" class="siteheader">PBK EXPRESS DOWNERS GROVE</a>' +
+              '<a href="http://www.google.com" class="siteaddress">2589 Ogden Ave Downers Grove, IL 60515</a>' +
+              '<a href="http://www.google.com" class="siteaddressbtn">LOCATION DETAILS</a>'
+        "</div>"
+      ;
 
       const infowindow = new google.maps.InfoWindow({
         content: contentString,
